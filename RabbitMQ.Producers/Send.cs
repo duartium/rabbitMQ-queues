2b818f21ab2e5c53e4611a1966a5ae9836ad4b3a
duartium <byronduarte95@gmail.com>
2024-10-07 12:03:54 -0500

    :sparkles: Implement communication mechanism between producers and receivers

---
+++ RabbitMQ.Producers/Send.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "orders",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        string messageToSend = $"Order added with price $874.82";
+        var body = Encoding.UTF8.GetBytes(messageToSend);
+
+        channel.BasicPublish(exchange: "",
+            routingKey: "orders",
+            basicProperties: null,
+            body: body);
+        Console.WriteLine($"[x] sent {messageToSend}");
+    }
+}
+
+Console.WriteLine("¡Perfect! press any key to continue (*.*)// ");
+Console.ReadLine();
---
+++ RabbitMQ.Receive/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "orders",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($"[x] Received {message}");
+        };
+
+        channel.BasicConsume(queue: "orders", autoAck: true, consumer: consumer);
+        Console.WriteLine("Receive: Presiona cualquier tecla para continuar...");
+        Console.ReadLine();
+    }
+}
+
+Console.WriteLine("¡Perfect! press any key to continue (*.*)// ");
+Console.ReadLine();
